650610fb017852786a879d592d4b20c0ca3123e2
Nitin Jarodia <nitin8816063850@gmail.com>
2026-08-02 10:50:07 +0530

    Time: 0 ms (100%), Space: 28.2 MB (72.17%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n  = nums.size();
+
+        int cnt = 0 ;
+        int candidate = 0;
+
+        for(int x : nums){
+            if(cnt == 0){
+                candidate = x;
+            }
+            if( x == candidate){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return candidate;
+    }
+};
