bfa2ea505a565228f351b25488b634b0eca304bc
Nitin Jarodia <nitin8816063850@gmail.com>
2026-08-05 20:25:11 +0530

    Time: 1056 ms (5.02%), Space: 60.6 MB (6.43%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        if(n > m) return false;
+
+        sort(s1.begin(), s1.end());
+
+        for(int i = 0 ; i <= m - n; i++){
+            
+            string window = s2.substr(i , n);
+
+
+            sort(window.begin() , window.end());
+
+            if(window == s1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
