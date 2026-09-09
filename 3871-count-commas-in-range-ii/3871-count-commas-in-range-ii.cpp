7e6d40384a22804f6a629f300d9a686bab859932
Nitin Jarodia <nitin8816063850@gmail.com>
2026-09-09 22:48:17 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+
+        long long ans = 0;
+
+      if(n<1000){
+        return 0;
+      } 
+      for(long long x = 1000 ; x<=n ; x*=1000){
+            ans += n-x +1;
+      }
+      return ans;
+    }
+};
